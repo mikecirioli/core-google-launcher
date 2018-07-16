50fb9b963c42c998f22bd9e7a8947626b0b19a4e
jefferyfry <jeff@jefferyfry.com>
2018-07-15 18:47:23 -0700

    Updated nginx yamls and applying with deploy.sh

--- deployer/deploy.sh
+++ deployer/deploy.sh
@@ -144,16 +144,7 @@ app_api_version=$(kubectl get "applications/$NAME" \
 
 create_manifests.sh
 
-###ingress-controller creation###
-# Assign owner references for the resources.
-/bin/set_ownership.py \
-  --app_name "$NAME" \
-  --app_uid "$app_uid" \
-  --app_api_version "$app_api_version" \
-  --manifests "/data/manifest-ingress-expanded" \
-  --dest "/data/ingress-controller.yaml"
-
-install_ingress_controller "/data/ingress-controller.yaml"
+install_ingress_controller "/data/manifest-ingress-expanded/nginx.yaml"
 
 ###cje###
 # Assign owner references for the resources.
